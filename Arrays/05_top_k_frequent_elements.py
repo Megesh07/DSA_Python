135175228ac510802403d1a71b6d03bf2772f321
Megeshwara S <megeshsundharaj@gmail.com>
2026-06-06 14:59:09 +0530

    Solve Top K Frequent Elements

---
+++ Arrays/05_top_k_frequent_elements.py
@@ -0,0 +1,21 @@
+"""
+Problem: Top K Frequent Elements
+Link: https://leetcode.com/problems/top-k-frequent-elements/
+
+Time Complexity: O(N)
+Space Complexity: O(N)
+"""
+class Solution:
+    def topKFrequent(self, nums: list[int], k: int) -> list[int]:
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+        for n, c in count.items():
+            freq[c].append(n)
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
