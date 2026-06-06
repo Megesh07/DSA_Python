1a1904075434dbd2fb7ef2ebebaae9f391b1aa4d
Megeshwara S <megeshsundharaj@gmail.com>
2026-06-06 14:59:09 +0530

    Solve Group Anagrams

---
+++ Arrays/04_group_anagrams.py
@@ -0,0 +1,17 @@
+"""
+Problem: Group Anagrams
+Link: https://leetcode.com/problems/group-anagrams/
+
+Time Complexity: O(M * N)
+Space Complexity: O(M * N)
+"""
+from collections import defaultdict
+class Solution:
+    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+        res = defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+            res[tuple(count)].append(s)
+        return list(res.values())
--- progress.md
+++ progress.md
@@ -2,4 +2,4 @@
 
 | Date | Topic | Problem | Status |
 |------|-------|---------|--------|
-| 2026-06-06 | Arrays | Valid Anagram | Complete |\n| 2026-06-06 | Arrays | Two Sum | Complete |\n
+| 2026-06-06 | Arrays | Valid Anagram | Complete |\n| 2026-06-06 | Arrays | Two Sum | Complete |\n| 2026-06-06 | Arrays | Group Anagrams | Complete |\n
