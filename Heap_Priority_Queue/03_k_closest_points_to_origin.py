25c6e974dd1a105221ffa6d75f5bc2108759e397
Megeshwara S <megeshsundharaj@gmail.com>
2026-06-06 15:11:12 +0530

    Solve K Closest Points to Origin

---
+++ Heap_Priority_Queue/03_k_closest_points_to_origin.py
@@ -0,0 +1,13 @@
+import heapq
+class Solution:
+    def kClosest(self, points: list[list[int]], k: int) -> list[list[int]]:
+        pts = []
+        for x, y in points:
+            dist = (abs(x) ** 2) + (abs(y) ** 2)
+            pts.append([dist, x, y])
+        res = []
+        heapq.heapify(pts)
+        for i in range(k):
+            dist, x, y = heapq.heappop(pts)
+            res.append([x, y])
+        return res
